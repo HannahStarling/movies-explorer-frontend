4ba6e7193ada7f7141f06738f43c3ae16ec99aca
Anna Tuban <mshannahstarling@gmail.com>
2022-04-27 01:11:52 +0300

    feat: add Avatar component

---
+++ src/components/Avatar/Avatar.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import foto from '../../images/foto.png';
+
+export const Avatar = ({ styles, ...props }) => {
+  return <img src={foto} alt={'Фотография-аватар автора сайта и владельца портфолио.'} className={styles} />;
+};
