fb4884d3ce0b387625ba388ac290f1bc64dcb96e
Anna Tuban <mshannahstarling@gmail.com>
2022-04-27 15:29:12 +0300

    fix: Header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,9 @@ import { AccountMenu } from '../Navigation/AccountMenu/AccountMenu';
 import { Logo } from '../Logo/Logo';
 import './Header.css';
 
-function Header() {
+function Header({ type = 'white' }) {
   return (
-    <header className={'header'}>
+    <header className={`header header_type_${type}`}>
       <Logo />
       <AccountMenu />
     </header>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,14 +4,20 @@ import { Promo } from '../Promo/Promo';
 import { Tech } from '../Tech/Tech';
 import { About } from '../About/About';
 import './Main.css';
+import Header from '../Header/Header';
+import { Footer } from '../Footer/Footer';
 
 export const Main = ({ colored }) => {
   return (
-    <main className={`main`}>
-      <Promo />
-      <AboutProject />
-      <Tech />
-      <About />
-    </main>
+    <>
+      <Header type='pink' />
+      <main className={`main`}>
+        <Promo />
+        <AboutProject />
+        <Tech />
+        <About />
+      </main>
+      <Footer />
+    </>
   );
 };
