dc54241c455dcc6eb83cf57ac8bfdda3918c8980
Anna Tuban <mshannahstarling@gmail.com>
2022-05-08 20:53:23 +0300

    feat: add Protected routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Account } from '../Account/Account';
+import { ProtectedRoute } from '../HOC/ProtectedRoute';
 import { Login } from '../Login/Login';
 import { Main } from '../Main/Main';
 import { Modal } from '../Modal/Modal';
@@ -10,15 +11,26 @@ import { Register } from '../Register/Register';
 import { SavedMovies } from '../SavedMovies/SavedMovies';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(true);
+
   return (
     <>
       <Routes>
         <Route path='/' element={<Main />} />
-        <Route path='/movies' element={<MoviesPage />} />
-        <Route path='/saved-movies' element={<SavedMovies />} />
-        <Route path='/profile' element={<Account />} />
         <Route path='/signup' element={<Register />} />
         <Route path='/signin' element={<Login />} />
+        <Route
+          path='/movies'
+          element={<ProtectedRoute loggedIn={loggedIn} component={<MoviesPage />} />}
+        />
+        <Route
+          path='/saved-movies'
+          element={<ProtectedRoute loggedIn={loggedIn} component={<SavedMovies />} />}
+        />
+        <Route
+          path='/profile'
+          element={<ProtectedRoute loggedIn={loggedIn} component={<Account />} />}
+        />
         <Route path='*' element={<NotFound />} />
       </Routes>
       <Modal />
