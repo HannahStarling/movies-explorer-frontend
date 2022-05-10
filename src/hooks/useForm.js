77dd4c84eb93b967eb9cb0425a3222c99613dc0c
Anna Tuban <mshannahstarling@gmail.com>
2022-05-10 11:56:27 +0300

    feat: add hook useForm

---
+++ src/hooks/useForm.js
@@ -0,0 +1,47 @@
+import { useState, useCallback } from 'react';
+
+const useForm = () => {
+  const [values, setValues] = useState({});
+  const [errors, setErrors] = useState({});
+  const [isValid, setIsValid] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value, type, checked, validationMessage } = e.target;
+    const values = type === 'checkbox' ? checked : value;
+
+    setValues((prevValues) => {
+      return {
+        ...prevValues,
+        [name]: values,
+      };
+    });
+
+    setErrors((prevErrors) => {
+      return {
+        ...prevErrors,
+        [name]: validationMessage,
+      };
+    });
+
+    setIsValid(e.target.closest('form').checkValidity());
+  };
+
+  const resetForm = useCallback(
+    (resetValues = {}, resetErrors = {}, resetIsValid = false) => {
+      setValues(resetValues);
+      setErrors(resetErrors);
+      setIsValid(resetErrors);
+    },
+    [setValues, setErrors, setIsValid],
+  );
+
+  return {
+    values,
+    errors,
+    isValid,
+    handleChange,
+    resetForm,
+  };
+};
+
+export default useForm;
