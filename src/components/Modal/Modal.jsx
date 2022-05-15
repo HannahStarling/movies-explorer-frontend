b7667e50170a72d52d8ae58f3b26b384d24d6c9f
Anna Tuban <mshannahstarling@gmail.com>
2022-05-16 00:55:34 +0300

    fix: show error Modal component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,16 +4,23 @@ import { Paragraph } from '../Paragraph/Paragraph';
 import { LinedTitle } from '../LinedTitle/LinedTitle';
 import './Modal.css';
 
-export const Modal = ({
-  error = { status: 'Ошибка!', text: 'Попробуйте повторить позденее' },
-  isOpen = false,
-}) => {
+export const Modal = ({ name, status, onClose, isOpen = false }) => {
+  // let text;
+
+  // switch (status) {
+  //   case value:
+  //     break;
+
+  //   default:
+  //     break;
+  // }
+
   return (
     <aside className={`modal ${isOpen ? 'modal_opened' : ''}`.trim()}>
       <div className={`modal__container ${isOpen ? 'modal__container_opened' : ''}`.trim()}>
-        <Button className='modal__btn' />
-        <LinedTitle title={error.status} className={'modal__title'} />
-        <Paragraph text={error.text} className={'modal__text'} />
+        <Button onClick={onClose} className='modal__btn' />
+        <LinedTitle title={status} className={'modal__title'} />
+        <Paragraph text={name} className={'modal__text'} />
       </div>
     </aside>
   );
