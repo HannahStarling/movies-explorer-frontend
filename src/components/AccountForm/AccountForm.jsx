2ded64673a0a75d6e9923222171621d220850a15
Anna Tuban <mshannahstarling@gmail.com>
2022-05-15 14:21:31 +0300

    feat: update account

--- src/components/AccountForm/AccountForm.jsx
+++ src/components/AccountForm/AccountForm.jsx
@@ -1,16 +1,66 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { CurrentUserContext } from '../../context/CurrentUserContext';
+import useForm from '../../hooks/useForm';
 import { Button } from '../Button/Button';
 import { Form } from '../Form/Form';
 import { Input } from '../Input/Input';
+import { Paragraph } from '../Paragraph/Paragraph';
 import './AccountForm.css';
 
-export const AccountForm = () => {
+export const AccountForm = ({ onLogOut, onUpdate }) => {
+  const [isEdited, setIsEdited] = useState(false);
+
+  const { name, email } = useContext(CurrentUserContext);
+  const { values, handleChange, resetForm } = useForm();
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    setIsEdited(true);
+    resetForm({ name, email });
+  };
+
+  const changeUserInfo = (e) => {
+    e.preventDefault();
+    onUpdate(values);
+    setIsEdited(false);
+  };
+
   return (
-    <Form name={'account'} className={'account__form'}>
-      <Input type='text' name={'name'} placeholder={'Имя'} className={'account'} />
-      <Input type='email' name={'email'} placeholder={'E-mail'} className={'account'} />
-      <Button type={'submit'} className='account__btn' text={'Редактировать'} />
+    <Form onSubmit={changeUserInfo} name={'account'} className={'account__form'}>
+      {isEdited ? (
+        <>
+          <Input
+            value={values.name}
+            onChange={handleChange}
+            type='text'
+            name={'name'}
+            placeholder={'Имя'}
+            className={'account'}
+          />
+          <Input
+            value={values.email}
+            onChange={handleChange}
+            type='email'
+            name={'email'}
+            placeholder={'E-mail'}
+            className={'account'}
+          />
+          <Button type={'submit'} className='account__btn' text={'Завершить редактирование'} />
+        </>
+      ) : (
+        <>
+          <Paragraph className='account__non-input'>{name}</Paragraph>
+          <Paragraph className='account__non-input'>{email}</Paragraph>
+          <Button
+            onClick={handleUpdate}
+            type={'button'}
+            className={'account__btn'}
+            text={'Редактировать'}
+          />
+        </>
+      )}
       <Button
+        onClick={onLogOut}
         type={'button'}
         className='account__btn account__btn_type_logout'
         text={'Выйти из аккаунта'}
--- src/utils/api/MainApi.js
+++ src/utils/api/MainApi.js
@@ -6,7 +6,8 @@ const { signin, signout, signup, users, movies } = ENDPOINTS;
 // GET /api/users/me возвращает информацию о пользователе (email и имя);
 export const getUser = () => request(MAIN_API_URL, { url: users, method: 'GET' });
 // PATCH /api/users/me — обновляет информацию о пользователе;
-export const updateUser = () => request(MAIN_API_URL, { url: users, method: 'PATCH' });
+export const updateUser = ({ name, email }) =>
+  request(MAIN_API_URL, { url: users, method: 'PATCH', body: { name, email } });
 // GET /api/movies все сохранённые пользователем фильмы;
 export const getMovies = () => request(MAIN_API_URL, { url: movies, method: 'GET' });
 // POST /api/movies создаёт фильм с переданными в теле данными;
@@ -15,10 +16,10 @@ export const createMovie = () => request(MAIN_API_URL, { url: movies });
 export const deleteMovie = (id) =>
   request(MAIN_API_URL, { url: `${movies}${id}`, method: 'DELETE' });
 // POST /api/signup создаёт пользователя с переданными в теле данными;
-export const register = (password, email) =>
-  request(MAIN_API_URL, { url: signup, body: { password, email } });
+export const register = (name, password, email) =>
+  request(MAIN_API_URL, { url: signup, body: { name, password, email } });
 //POST вход пользователя
 export const authorize = (password, email) =>
   request(MAIN_API_URL, { url: signin, body: { password, email } });
 // DELETE /api/signout выход пользователя;
-export const signOut = () => request(MAIN_API_URL, { url: signout, method: 'DELETE' });
+export const logOut = () => request(MAIN_API_URL, { url: signout, method: 'DELETE' });
