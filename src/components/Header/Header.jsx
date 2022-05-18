8cd4acb1f2ea54316a26ab2062eb93b560d7d3ad
Anna Tuban <mshannahstarling@gmail.com>
2022-05-18 23:05:57 +0300

    delete: unused imports

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { Menu } from '../Navigation/Menu/Menu';
 import { BurgerBtn } from '../BurgerBtn/BurgerBtn';
 import { MobileNavigation } from '../Navigation/MobileNavigation/MobileNavigation';
